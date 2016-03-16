6268433f6253d2fa64798643a8e40efea62024a0
Matthias Van Gestel <maregt0@gmail.com>
2016-03-16 10:53:16 +0100

    topics.h repareren

--- src/topics.h
+++ src/topics.h
@@ -1,7 +1,3 @@
 #define robotposition 		"position"
 #define otherrobotposition 	"otherposition"
-<<<<<<< HEAD
 #define pathplanning		"pathplanning"
-=======
-#define pathplanning        "pathplanning"  
->>>>>>> e5a43353128c831315933a44e1ff285fbdf78251
